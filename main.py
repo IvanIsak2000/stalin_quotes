1ecc5a14a41357cb59402f50d5dbc0acfccea354
mb6ockatf <mb6ockatf@relay.firefox.com>
2023-04-30 00:42:18 +0300

    refactor

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
 import secrets
 
-def stalin(language,person) -> str:
 
-    if person =='S':
-
-        if language =='ru':
-
-            with open(f'stalin_ru.txt','r',encoding='utf-8') as file:
+def stalin(language: str, person: str):
+    if person == 'S':
+        if language == 'ru':
+            with open('stalin_ru.txt', 'r', encoding='utf-8') as file:
                 text = file.read()
-                text = text.split('/') 
-                full = 0 
+                text = text.split('\n')
+                full = 0
                 for c in text:
-                    full +=1
-               
-                print(f'\n{secrets.choice(text)}\n--И.В.Сталин.',full)
-
-        if language =='en':
-
-            with open(f'stalin_en.txt','r',encoding='utf-8') as file:
+                    full += 1
+                print(f'\n{secrets.choice(text)}\n--И.В.Сталин.', full)
+        if language == 'en':
+            with open('stalin_en.txt', 'r', encoding='utf-8') as file:
                 text = file.read()
-                text = text.split('/')
+                text = text.split('\n')
                 print(f'\n{secrets.choice(text)} \n--J.V.Stalin.')
 
 
-stalin('ru','S')
-
-  
+stalin('ru', 'S')

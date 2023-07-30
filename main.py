2a7d9ba4190095739f56f06c81216d0afacb6038
Iwan <vingving648@gmail.com>
2023-07-30 22:36:09 +0300

    fixed quote number, renamed func, added returns

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from random import choice
 
 
-def stalin(language: str, person: str):
+def get_quote(language: str, person: str) -> str | None:
     if person != 'S':
-        return
+        return None
     if language == 'ru':
         with open('stalin_ru.txt', 'r', encoding='utf-8') as file:
             text = file.read()
             text = text.split('\n')
-            print(f'\n{choice(text)}\n--И.В.Сталин.', len(text))
+            random_quotes = choice(text)
+            return print(f'\n{random_quotes}\n--И.В.Сталин. \n{text.index(random_quotes)}')
+           
     if language == 'en':
         with open('stalin_en.txt', 'r', encoding='utf-8') as file:
             text = file.read()
             text = text.split('\n')
-            print(f'\n{choice(text)} \n--J.V.Stalin.')
+            random_quotes = choice(text)
+            return print(f'\n{choice(text)} \n--J.V.Stalin.\n{text.index(random_quotes)}')
 
 
 if __name__ == "__main__":
-    stalin('ru', 'S')
+    get_quote('en', 'S')

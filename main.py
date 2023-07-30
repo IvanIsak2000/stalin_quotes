856167829f24068f9a31d052fe53aa33943f6b41
mb6ockatf <mdddmmmm@ya.ru>
2023-07-30 21:50:13 +0300

    fix: typos, small optimisations

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import secrets
+from random import choice
 
 
 def stalin(language: str, person: str):
-    if person == 'S':
-        if language == 'ru':
-            with open('stalin_ru.txt', 'r', encoding='utf-8') as file:
-                text = file.read()
-                text = text.split('\n')
-                full = 0
-                for c in text:
-                    full += 1
-                print(f'\n{secrets.choice(text)}\n--И.В.Сталин.', full)
-        if language == 'en':
-            with open('stalin_en.txt', 'r', encoding='utf-8') as file:
-                text = file.read()
-                text = text.split('\n')
-                print(f'\n{secrets.choice(text)} \n--J.V.Stalin.')
+    if person != 'S':
+        return
+    if language == 'ru':
+        with open('stalin_ru.txt', 'r', encoding='utf-8') as file:
+            text = file.read()
+            text = text.split('\n')
+            print(f'\n{choice(text)}\n--И.В.Сталин.', len(text))
+    if language == 'en':
+        with open('stalin_en.txt', 'r', encoding='utf-8') as file:
+            text = file.read()
+            text = text.split('\n')
+            print(f'\n{choice(text)} \n--J.V.Stalin.')
 
 
-stalin('ru', 'S')
+if __name__ == "__main__":
+    stalin('ru', 'S')
